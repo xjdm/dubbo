b8f7f66966eedd7e1709d10641b1fd8fd476519d
xjdm <1121245860@qq.com>
2020-09-06 21:46:21 +0800

    Demo API 注释

--- dubbo-demo/dubbo-demo-api/dubbo-demo-api-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
+++ dubbo-demo/dubbo-demo-api/dubbo-demo-api-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
@@ -38,16 +38,19 @@ public class Application {
     }
 
     private static void runWithBootstrap() {
+        // 创建ReferenceConfig,其中制定了引用的接口DemoService
         ReferenceConfig<DemoService> reference = new ReferenceConfig<>();
         reference.setInterface(DemoService.class);
         reference.setGeneric("true");
 
+        // 创建DubboBootstrap，指定ApplicationConfig以及RegistryConfig
         DubboBootstrap bootstrap = DubboBootstrap.getInstance();
         bootstrap.application(new ApplicationConfig("dubbo-demo-api-consumer"))
                 .registry(new RegistryConfig("zookeeper://127.0.0.1:2181"))
                 .reference(reference)
                 .start();
 
+        // 获取DemoService实例并调用其方法
         DemoService demoService = ReferenceConfigCache.getCache().get(reference);
         String message = demoService.sayHello("dubbo");
         System.out.println(message);
--- dubbo-demo/dubbo-demo-api/dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
+++ dubbo-demo/dubbo-demo-api/dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
@@ -38,11 +38,16 @@ public class Application {
     }
 
     private static void startWithBootstrap() {
+        // 创建一个ServiceConfig 的实例，泛型参数是业务接口实现列 ，即DemoServiceImpl
         ServiceConfig<DemoServiceImpl> service = new ServiceConfig<>();
+        // 指定业务接口
         service.setInterface(DemoService.class);
+        // 指定业务接口的实现，由该对象来处理Consumer的请求
         service.setRef(new DemoServiceImpl());
 
+        // 获取DubboBootstrap的实例，这是个单例的对象
         DubboBootstrap bootstrap = DubboBootstrap.getInstance();
+        // 生产一个ApplicationConfig 的实例，指定ZK地址以及ServiceConfig实例
         bootstrap.application(new ApplicationConfig("dubbo-demo-api-provider"))
                 .registry(new RegistryConfig("zookeeper://127.0.0.1:2181"))
                 .service(service)
